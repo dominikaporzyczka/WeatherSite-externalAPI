9d75405dc3e78b5287ef96ca40e324dca65123d6
Dominika Porzyczka <dominikaporzyczka@gmail.com>
2018-04-19 20:04:51 +0200

    Add request for forecast API

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 const xhrConditions = new XMLHttpRequest();
+const xhrForecast = new XMLHttpRequest();
 let conditionsObj;
+let forecastObj;
 
+// CONDITIONS
 xhrConditions.open('GET', 'http://api.wunderground.com/api/e17b6fcb2c4b30bc/conditions/q/CA/San_Francisco.json', true)
 xhrConditions.responseType = 'text';
 xhrConditions.send();
@@ -8,10 +11,22 @@ xhrConditions.send();
 xhrConditions.onload = function () {
     if (xhrConditions.status === 200) {
         conditionsObj = JSON.parse(xhrConditions.responseText);
-        console.log(conditionsObj);
 
         document.getElementById('location').innerHTML = conditionsObj.current_observation.display_location.full;
         document.getElementById('weather').innerHTML = conditionsObj.current_observation.weather;
         document.getElementById('temperature').innerHTML = conditionsObj.current_observation.temp_c;
+    }
+}
+
+// FORECAST
+xhrForecast.open('GET', 'http://api.wunderground.com/api/e17b6fcb2c4b30bc/forecast/q/CA/San_Francisco.json', true)
+xhrForecast.responseType = 'text';
+xhrForecast.send();
+
+xhrForecast.onload = function () {
+    if (xhrForecast.status === 200) {
+        forecastObj = JSON.parse(xhrForecast.responseText);
+
+        document.getElementById('description').innerHTML = forecastObj.forecast.txt_forecast.forecastday[0].fcttext;
     }
 }
